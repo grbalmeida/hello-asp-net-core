52c0c6df2fb2b6576dd2ed0737abb88190dc64a4
root <g.r.almeida@live.com>
2019-07-31 18:11:45 -0300

    Editing departments

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+using InstitutionOfHigherEducation.Models;
 using InstitutionOfHigherEducation.Data;
 
 namespace InstitutionOfHigherEducation.Controllers
@@ -19,6 +20,32 @@ namespace InstitutionOfHigherEducation.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Departments.OrderBy(departament => departament.Name).ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name")] Department department)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(department);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch(DbUpdateException)
+            {
+                ModelState.AddModelError("", "Could not enter data");
+            }
+
+            return View(department);
         }
     }
 }
